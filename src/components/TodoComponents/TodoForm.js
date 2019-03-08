74d2a9ddb39484f90af1c68c39cd45bf9c973277
David <digital0david@gmail.com>
2019-03-07 23:53:03 -0500

    button css

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,8 +4,8 @@ function TodoForm(props) {
     return (
         <form onSubmit={ props.addItemHandler }>
             <input type="text" name="task" placeholder="...to-do" value={props.task} onChange={props.inputHandler}/> 
-            <button type="submit">Add To-do</button>
-            <button onClick={props.clearCompleted}>Clear Completed</button>
+            <button className="buttons" type="submit">Add To-do</button>
+            <button className="buttons" onClick={props.clearCompleted}>Clear Completed</button>
         </form>
     )
 }
